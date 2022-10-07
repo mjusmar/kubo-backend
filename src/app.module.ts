70d4736027a70ee686728f27cbe250f71449a40d
mjusmar <devjunior6titanq@gmail.com>
2022-10-07 16:45:54 -0500

    feat: adding typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'postgres',
+      password: 'password123',
+      database: 'kubo',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: false,
+      retryDelay: 3000,
+      retryAttempts: 10
+    }),
+  ],
   controllers: [],
   providers: [],
 })
